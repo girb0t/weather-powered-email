8d174c3a6648e046c9cf0120b5f24236637f6c1d
Pei Xiong Liu <peixiong.liu@gmail.com>
2017-04-25 16:25:25 -0400

    Handle errors when saving to users table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    binding.pry
+    begin
+      user = User.create!(
+        email: params[:email],
+        city: City.find(params[:city_id])
+      )
+    rescue => e
+      user_message = "Oops, something when wrong."
+
+      if e.message == "Validation failed: Email is invalid"
+        user_message = "Please use a correctly formatted email!"
+      elsif e.class == ActiveRecord::RecordNotUnique && e.message.include?("users_email_key")
+        user_message = "'#{params[:email]}' already in use!"
+      end
+
+      redirect_to root_path, :flash => { :danger => user_message }
+    end
   end
 end
