b49e681bb46ee72648a8d1cdcdcf9aa41409f700
Pei Xiong Liu <peixiong.liu@gmail.com>
2017-04-26 17:12:34 -0400

    Add form control feedback messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,8 +38,13 @@ $(function() {
     }
   })
 
+  $('#email-input').blur(function(event) {
+    validateEmail(event.target.value);
+  })
+
   function onSubmitSuccess(response) {
-    alert('success!')
+    alert('success!');
+    resetForm();
   }
 
   function onSubmitError(response) {
@@ -47,10 +52,63 @@ $(function() {
   }
 
   function validateEmail(email) {
-    return true;
+    var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (regex.test(email)) {
+      showValidEmailMsg()
+      return true;
+    } else {
+      showInvalidEmailMsg();
+      return false;
+    }
   }
 
   function validateCity(value) {
-    return true;
+    if (value === 'EMPTY') {
+      showInvalidLocationMsg()
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function showInvalidEmailMsg() {
+    clearEmailFormControlMsg();
+    var $errorMsg = $('.form-group.email .form-control-msg.error');
+    $errorMsg.show().text('Invalid format.');
+  }
+
+  function showValidEmailMsg() {
+    clearEmailFormControlMsg();
+    var $success_msg = $('.form-group.email .form-control-msg.success');
+    $success_msg.show().text('Good email.');
+  }
+
+  function showInvalidLocationMsg() {
+    $errorMsg = $('.form-group.location .form-control-msg.error');
+    $errorMsg.show().text('Please choose a location.');
+  }
+
+  function resetForm() {
+    clearFormInput();
+    clearAllFormControlMsg();
+  }
+
+  function clearFormInput() {
+    $('#email-input').val('');
+    $('#city-select').val('EMPTY');
+  }
+
+  function clearAllFormControlMsg() {
+    clearEmailFormControlMsg();
+    clearLocationFormControlMsg();
+  }
+
+  function clearEmailFormControlMsg() {
+    $('.form-group.email .form-control-msg.error').hide();
+    $('.form-group.email .form-control-msg.success').hide();
+  }
+
+  function clearLocationFormControlMsg() {
+    $('.form-group.location .form-control-msg.error').hide();
   }
 });
