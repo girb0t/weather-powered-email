b2aa3e637cda6c3ff8780e72df2b310b52b82464
Pei Xiong Liu <peixiong.liu@gmail.com>
2017-04-27 15:57:10 -0400

    Change app name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_klaviyo-weather-email_session'
+Rails.application.config.session_store :cookie_store, key: '_weather-powered-email_session'
