017047a73f1d4f8731178c3a6dcd550ea96fc817
Pei Xiong Liu <peixiong.liu@gmail.com>
2017-04-27 15:06:51 -0400

    Create self.weather_feel method for WundergroundApiClient

--- app/process/wunderground_api_client.rb
+++ app/process/wunderground_api_client.rb
@@ -1,8 +1,38 @@
 require 'rest-client'
 class WundergroundApiClient
   API_KEY= '199d199e57a50a1e'
-
   BASE_URL = 'https://api.wunderground.com/api/' + API_KEY
+
+  # Current conditions from WU that have precipitation will contain the following words.
+  # Note that volcanic ash, sand/dust storms do not qualify as 'precipitation' according to
+  # the meteorological definition of the word.
+  # Source: https://www.wunderground.com/weather/api/d/docs?d=resources/phrase-glossary&MR=1
+  PRECIPITATION_GLOSSARY = [
+    'Drizzle',
+    'Rain',
+    'Snow',
+    'Ice',
+    'Hail',
+    'Thunderstorm',
+    'Squalls',
+    'Precipitation'
+  ].freeze
+
+  # Takes objective weather information and returns a subjective descriptor of the weather:
+  # :good - (Clear && not below-avg temp) || (No Precipitation && above-avg temp)
+  # :bad  - (Pricipitation || below-avg temp)
+  # :average - all other cases
+  def self.weather_feel(current_temp, avg_temp, condition)
+    relative_temp = relative_temp(current_temp, avg_temp)
+
+    if ((condition == 'Clear') && relative_temp != :below_avg) || (!precipitating?(condition) && relative_temp == :above_avg)
+      :good
+    elsif precipitating?(condition) || relative_temp == :below_avg
+      :bad
+    else
+      :average
+    end
+  end
 
   def self.conditions(city, state)
     raw_json = conditions_raw(city, state)
@@ -27,7 +57,8 @@ class WundergroundApiClient
     get("#{BASE_URL}/almanac/q/#{state}/#{format_city(city)}.json")
   end
 
-  private
+
+  # Helper methods
 
   def self.get(url)
     response = try_cache(url) do
@@ -47,5 +78,22 @@ class WundergroundApiClient
 
   def self.format_city(city)
     city.gsub(' ', '_')
+  end
+
+  def self.relative_temp(current_temp, avg_temp)
+    if current_temp >= avg_temp + 5
+      :above_avg
+    elsif current_temp <= avg_temp - 5
+      :below_avg
+    else
+      :average
+    end
+  end
+
+  def self.precipitating?(condition)
+    PRECIPITATION_GLOSSARY.each do |precip|
+      return true if condition.include?(precip)
+    end
+    return false
   end
 end
